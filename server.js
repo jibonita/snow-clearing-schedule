f0eaecf6d09d1340fa03813054be957f4c5dc0f3
jibonita <bamishore@gmail.com>
2020-02-22 19:29:51 +0200

    add app get

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
+app.get('/*', function(req, res) {
+   res.sendFile(
+      path.join(__dirname + '/dist/snow-clearing-schedule/index.html')
+   );
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, (req, res) => {
    console.log(`Server running on port ${port}`);
-})
+});
